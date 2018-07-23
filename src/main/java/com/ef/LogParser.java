73f370db8be71a4898ee44813f0844419963e671
Jacob Boswell <jacob@s56.net>
2018-07-23 10:42:05 -0600

    got arg validation done

--- src/main/java/com/ef/LogParser.java
+++ src/main/java/com/ef/LogParser.java
@@ -3,22 +3,9 @@ package com.ef;
 import org.apache.commons.cli.CommandLine;
 
 public class LogParser {
-    private CommandLine line;
-    public LogParser() {}
 
-    public LogParser(CommandLine line) {
-        this.line = line;
-    }
-
-    public String run() {
+    public String run(CommandLineArgs args) {
         return "cool n stuffz";
     }
 
-    public CommandLine getLine() {
-        return line;
-    }
-
-    public void setLine(CommandLine line) {
-        this.line = line;
-    }
 }
--- src/main/java/com/ef/Parser.java
+++ src/main/java/com/ef/Parser.java
@@ -19,10 +19,10 @@ public class Parser {
         try {
             // parse the command line arguments
             CommandLine line = parser.parse(options, args);
-            LogParser logParser = new LogParser(line);
-            String output = logParser.run();
+            LogParser logParser = new LogParser();
+            String output = logParser.run(parseOptions(line));
             System.out.print(output);
-        } catch (ParseException exp) {
+        } catch (ParseException | java.text.ParseException exp) {
             // oops, something went wrong
             System.err.println("Parsing failed.  Reason: " + exp.getMessage());
             HelpFormatter formatter = new HelpFormatter();
@@ -30,9 +30,14 @@ public class Parser {
         }
     }
 
-    private static CommandLineArgs parseOptions(CommandLine line) throws java.text.ParseException {
+    private static CommandLineArgs parseOptions(CommandLine line) throws java.text.ParseException, ParseException {
         CommandLineArgs clArgs = new CommandLineArgs();
         clArgs.setStartDate(new SimpleDateFormat("yyyy-MM-dd.HH:mm:ss").parse(line.getOptionValue(OPT_START_DATE)));
+        clArgs.setThreshold(Integer.parseInt(line.getOptionValue(OPT_THRESHOLD)));
+        if (!line.getOptionValue(OPT_DURATION).matches("^hourly|daily$")) {
+            throw new ParseException("threshold MUST be either hourly or daily");
+        }
+        clArgs.setDuration(line.getOptionValue(OPT_DURATION));
         return clArgs;
     }
 
@@ -42,7 +47,7 @@ public class Parser {
                 Option.builder()
                         .argName(OPT_START_DATE)
                         .required()
-                        .desc("start date for search")
+                        .desc("start date for search, must be format yyyy-MM-dd.HH:mm:ss. Example: 2017-01-01.13:00:00")
                         .longOpt(OPT_START_DATE)
                         .hasArg()
                         .build()
@@ -52,7 +57,7 @@ public class Parser {
                 Option.builder()
                         .argName(OPT_DURATION)
                         .required()
-                        .desc("duration of search")
+                        .desc("duration of search, must be either 'hourly' or 'daily'")
                         .longOpt(OPT_DURATION)
                         .hasArg()
                         .build()
@@ -63,7 +68,7 @@ public class Parser {
                 Option.builder()
                         .argName(OPT_THRESHOLD)
                         .required()
-                        .desc("threshold for search")
+                        .desc("threshold for search, must be integer")
                         .longOpt(OPT_THRESHOLD)
                         .hasArg()
                         .build()
